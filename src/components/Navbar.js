1022e00ae8bb1206939f0f0b7dc66b9276187fc9
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-08-05 15:25:41 +0530

    changed navbar position to top for large device and bottom for small devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,21 +15,21 @@ function Navbar() {
                 </div>
             </nav>
             <div className="">
-                <section id="bottom-navigation" className="block fixed inset-x-0 bottom-0 z-10 bg-white shadow">
-                    <div id="tabs" className="flex justify-between">
-                        <div className="w-full grid grid-cols-1 text-lg bg-slate-700 text-slate-100 focus:text-indigo-200 hover:text-violet-400 justify-center text-center pt-2 pb-1">
+                <section id="bottom-navigation" className="block fixed inset-x-0 bottom-0 lg:relative lg:mt-10 lg:top-8 z-10 shadow">
+                    <div id="tabs" className="flex justify-between lg:fixed w-full">
+                        <div className="w-full grid grid-cols-1 text-lg bg-slate-700 lg:bg-indigo-200 text-slate-100 lg:text-slate-800 focus:text-indigo-200 hover:text-violet-400 justify-center text-center pt-2 pb-1">
                             <NavLink to="/" onClick={() => setNavbarOpen(!navbarOpen)} className="focus:text-indigo-200">
                                 <div><i className="fa-solid fa-house-user"></i></div> Home</NavLink>
                         </div>
-                        <div className="w-full text-lg bg-slate-700 text-slate-100 focus:text-indigo-200 hover:text-violet-400 justify-center inline-block text-center pt-2 pb-1">
+                        <div className="w-full text-lg bg-slate-700 lg:bg-indigo-200 text-slate-100 lg:text-slate-800 focus:text-indigo-200 hover:text-violet-400 justify-center inline-block text-center pt-2 pb-1">
                             <NavLink to="/search" onClick={() => setNavbarOpen(!navbarOpen)} className="focus:text-indigo-200">
                                 <div><i className="fa-solid fa-magnifying-glass"></i></div>Search</NavLink>
                         </div>
-                        <div className="w-full text-lg bg-slate-700 text-slate-100 focus:text-indigo-200 hover:text-violet-400 justify-center inline-block text-center pt-2 pb-1">
+                        <div className="w-full text-lg bg-slate-700 lg:bg-indigo-200 text-slate-100 lg:text-slate-800 focus:text-indigo-200 hover:text-violet-400 justify-center inline-block text-center pt-2 pb-1">
                             <NavLink to="/contact" onClick={() => setNavbarOpen(!navbarOpen)} className="focus:text-indigo-200">
                                 <div><i className="fa-solid fa-address-card"></i></div>Contact</NavLink>
                         </div>
-                        <div className="w-full text-lg bg-slate-700 text-slate-100 focus:text-indigo-200 hover:text-violet-400 justify-center inline-block text-center pt-2 pb-1">
+                        <div className="w-full text-lg bg-slate-700 lg:bg-indigo-200 text-slate-100 lg:text-slate-800 focus:text-indigo-200 hover:text-violet-400 justify-center inline-block text-center pt-2 pb-1">
                             <NavLink to="/about" onClick={() => setNavbarOpen(!navbarOpen)} className="focus:text-indigo-200">
                                 <div><i className="fa-solid fa-users"></i></div>About</NavLink>
                         </div>
